ca72dfbe9c0e50179e71f3ed23947e165fe7acd3
ncp601 <ncp601@mun.ca>
2017-03-31 01:15:43 -0230

    Add files via upload

--- src/Factories/LaundryComponentFactory.java
+++ src/Factories/LaundryComponentFactory.java
@@ -1,6 +1,6 @@
 package Factories;
 
-import FloorComponent.FloorComponent;
+import FloorComponent.*;
 
 public class LaundryComponentFactory extends AbstractFloorComponentFactory{
 	
@@ -10,14 +10,82 @@ public class LaundryComponentFactory extends AbstractFloorComponentFactory{
 			return null;
 		}
 		
+		else if(componentType.equals("WASHERFD")){
+			return new WasherComponentFacingDown(true);
+		}
+		
+		else if(componentType.equals("WASHERFU")){
+			return new WasherComponentFacingUp(true);
+		}
+		
+		else if(componentType.equals("WASHERFR")){
+			return new WasherComponentFacingRight(true);
+		}
+		
+		else if(componentType.equals("WASHERFL")){
+			return new WasherComponentFacingLeft(true);
+		}
+		
+		else if(componentType.equals("DRYERFD")){
+			return new DryerComponentFacingDown(true);
+		}
+		
+		else if(componentType.equals("DRYERFU")){
+			return new DryerComponentFacingUp(true);
+		}
+		
+		else if(componentType.equals("DRYERFR")){
+			return new DryerComponentFacingRight(true);
+		}
+		
+		else if(componentType.equals("DRYERFL")){
+			return new DryerComponentFacingLeft(true);
+		}
+		
 		else
 			return null;
 	}
 
 	@Override
 	public FloorComponent getGridComponent(String componentType) {
-		// TODO Auto-generated method stub
-		return null;
+		if(componentType == null){
+			return null;
+		}
+		
+		else if(componentType.equals("WASHERFD")){
+			return new WasherComponentFacingDown(false);
+		}
+		
+		else if(componentType.equals("WASHERFU")){
+			return new WasherComponentFacingUp(false);
+		}
+		
+		else if(componentType.equals("WASHERFR")){
+			return new WasherComponentFacingRight(false);
+		}
+		
+		else if(componentType.equals("WASHERFL")){
+			return new WasherComponentFacingLeft(false);
+		}
+		
+		else if(componentType.equals("DRYERFD")){
+			return new DryerComponentFacingDown(false);
+		}
+		
+		else if(componentType.equals("DRYERFU")){
+			return new DryerComponentFacingUp(false);
+		}
+		
+		else if(componentType.equals("DRYERFR")){
+			return new DryerComponentFacingRight(false);
+		}
+		
+		else if(componentType.equals("DRYERFL")){
+			return new DryerComponentFacingLeft(false);
+		}
+		
+		else
+			return null;
 	}
 	
 }
